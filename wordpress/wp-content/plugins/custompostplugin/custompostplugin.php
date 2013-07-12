43bbb6068b391a96e0df3b1657a65ecb08c43af9
med58 <med58g@gmail.com>
2013-07-12 11:40:00 -0400

    Finished deleting
    deleted the rest of it

--- wordpress/wp-content/plugins/custompostplugin/custompostplugin.php
+++ wordpress/wp-content/plugins/custompostplugin/custompostplugin.php
@@ -62,50 +62,6 @@ function add_tp_meta_boxes() {
 
 function tp_ratings_meta_box() { 
 
-	<?php wp_nonce_field( basename( __FILE__ ), 'tp_ratings_nonce' ); ?>
-
-	<p> 
-		<label for="tp-ratings"><?php _e( "Add the rating of the video.", 'example' ); ?></label>
-		<br />
-		<input class="widefat" type="text" name="tp-ratings" id="tp-ratings" value="<?php echo esc_attr( get_post_meta( $object->ID, 'tp_ratings', true ) ); ?>" size="30" />
-	</p>
-<?php }
-
-/* Save the meta box's data. */
-function tp_ratings_save_meta( $post_id, $post ) {
-
-	/* Verify the nonce before proceeding. */
-	if ( !isset( $_POST['tp_ratings_nonce'] ) || !wp_verify_nonce( $_POST['tp_ratings_nonce'], basename( __FILE__ ) ) )
-		return $post_id;
-
-	/* Get the post type object. */
-	$post_type = get_post_type_object( $post->post_type );
-
-	/* Check if the current user has permission to edit the post. */
-	if ( !current_user_can( $post_type->cap->edit_post, $post_id ) )
-		return $post_id;
-
-	/* Get the posted data and sanitize it for use as an HTML class. */
-	$new_meta_value = ( isset( $_POST['tp-ratings'] ) ? sanitize_html_class( $_POST['tp-ratings'] ) : '' );
-
-	/* Get the meta key. */
-	$meta_key = 'tp_ratings';
-
-	/* Get the meta value of the custom field key. */
-	$meta_value = get_post_meta( $post_id, $meta_key, true );
-
-	/* If a new meta value was added and there was no previous value, add it. */
-	if ( $new_meta_value && '' == $meta_value )
-		add_post_meta( $post_id, $meta_key, $new_meta_value, true );
-
-	/* If the new meta value does not match the old value, update it. */
-	elseif ( $new_meta_value && $new_meta_value != $meta_value )
-		update_post_meta( $post_id, $meta_key, $new_meta_value );
-
-	/* If there is no new meta value but an old value exists, delete it. */
-	elseif ( '' == $new_meta_value && $meta_value )
-		delete_post_meta( $post_id, $meta_key, $meta_value );
-} 
 
 
 
